cf1c4dbc2ef077fa846ad20b0abc806ce55c6aba
oaxiom <oaxiom@gmail.com>
2024-10-11 19:35:25 +0800

    add platform-specific code

---
+++ bin/glurm/platform.py
@@ -0,0 +1,17 @@
+
+# From psutil:
+
+import sys
+import os
+
+POSIX = os.name == "posix"
+WINDOWS = os.name == "nt"
+LINUX = sys.platform.startswith("linux")
+MACOS = sys.platform.startswith("darwin")
+OSX = MACOS  # deprecated alias
+FREEBSD = sys.platform.startswith(("freebsd", "midnightbsd"))
+OPENBSD = sys.platform.startswith("openbsd")
+NETBSD = sys.platform.startswith("netbsd")
+BSD = FREEBSD or OPENBSD or NETBSD
+SUNOS = sys.platform.startswith(("sunos", "solaris"))
+AIX = sys.platform.startswith("aix")
